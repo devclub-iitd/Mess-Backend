052149d8fb7c717b800460724b2014de04872550
AdityaRanjanSinha <adityaranjan19oct@gmail.com>
2025-01-11 16:22:12 +0530

    Routes Created

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,26 @@
 import { Button } from './components/ui/button'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home.tsx';
+import Dashboard from './pages/dashboard.tsx';
+import UserManagement from './pages/user_management.tsx';
+import Meal from './pages/meal.tsx';
+import Rebate from './pages/rebate.tsx';
+import Consumption from './pages/consumption.tsx';
 
 function App() {
-  return (
-   <Button size={"lg"}>Hello</Button>
-  )
+  // return (
+  //  <Button size={"lg"}>Hello</Button>
+  // )
+  <div className='App'>
+		<Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+		    <Route path='/usermanagement' element={<UserManagement />} />
+        <Route path='/meal' element={<Meal />} />
+        <Route path='/rebate' element={<Rebate />} />
+        <Route path='/consumption' element={<Consumption />} />
+    </Routes>
+		</div>
 }
 
 export default App
---
+++ web/src/pages/rebate.tsx
@@ -0,0 +1,7 @@
+const Rebate = () => {
+    return(
+        <div></div>
+    )
+};
+
+export default Rebate;
