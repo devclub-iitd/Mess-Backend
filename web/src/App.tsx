576315162f10e3a930f627414c5ca8d9430fba46
AdityaRanjanSinha <adityaranjan19oct@gmail.com>
2025-01-12 21:32:25 +0530

    Routing improved

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
-// import { Button } from './components/ui/button'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/home.tsx';
-import Login from './pages/login.tsx';
-// import Dashboard from './pages/dashboard.tsx';
-// import UserManagement from './pages/user_management.tsx';
-// import Meal from './pages/meal.tsx';
-// import Rebate from './pages/rebate.tsx';
-// import Consumption from './pages/consumption.tsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Login from "./pages/login";
+import Dashboard from "./pages//dashboard";
+import UserManagement from "./pages/user_management";
+import Meal from "./pages/meal";
+import Consumption from "./pages/consumption";
+import Rebate from "./pages/rebate";
 
 function App() {
   // return (
@@ -15,16 +15,32 @@ function App() {
   
   return(
     <Router>
-      <div className='App'>
-        <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/home' element={<Home />} />
-        </Routes>
+      <div style={{ display: "flex" }}>
+        <SidebarWrapper />
+        {/* Main content */}
+        <div style={{ flex: 1}}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/user-management" element={<UserManagement />} />
+            <Route path="/meal" element={<Meal />} />
+            <Route path="/consumption" element={<Consumption />} />
+            <Route path="/rebate" element={<Rebate />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   )
   
 }
+
+const SidebarWrapper = () => {
+  const location = useLocation();
+  const hideSidebarRoutes = ["/"]; // Routes where the sidebar is hidden
+  const showSidebar = !hideSidebarRoutes.includes(location.pathname);
+
+  return showSidebar ? <Sidebar /> : null;
+};
 
 export default App
 
