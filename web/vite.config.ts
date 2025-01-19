dbda7002260d5e75bffd36509b4f4f0c8cf300dc
Muskan <ms1230266@iitd.ac.in>
2025-01-19 18:10:00 +0530

    chnages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": "https://mess-backend-otjk.onrender.com/api/",
+    },
+  }
 })
